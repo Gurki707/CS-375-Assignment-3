5a54bf8f80a459d62fce05a785f5f0f4782ff2a3
Gurki707 <159870471+Gurki707@users.noreply.github.com>
2024-10-21 15:12:35 -0700

    Update BasicCube.js

--- BasicCube.js
+++ BasicCube.js
@@ -8,7 +8,7 @@
 class BasicCube {
     constructor(gl, vertexShader, fragmentShader) {
 
-        // let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+        let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
 
         this.draw = () => {
             // program.use();
